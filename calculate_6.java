44b72bfcb8ba1f5f21ccc38bde670a6244bc78b4
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:12:46 +0900

    ランダム関数

---
+++ calculate_6.java
@@ -0,0 +1,6 @@
+public class calculate_6 {
+    public static void main(String[] args){
+        int a = new java.util.Random().nextInt(100);
+        System.out.println("モリタの年齢は"+a+"才です。");
+    }
+}
