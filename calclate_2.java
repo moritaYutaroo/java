7281daa78b234a6054d5cd4296fb35ad455a8ab5
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:11:25 +0900

    四則演算2

---
+++ calclate_2.java
@@ -0,0 +1,8 @@
+public class calclate_2 {
+    public static void main(String[] args){
+        double d = 4.32/23;
+        long l = 5 + 3L;
+        System.out.println("4.32/23 = " + d);
+        System.out.println("5 + 3L = " + l);
+    }
+}
