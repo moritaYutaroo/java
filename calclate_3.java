5e20ed701dff195458836ab43fec1ffd175a8705
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:11:40 +0900

    文字列の結合

---
+++ calclate_3.java
@@ -0,0 +1,8 @@
+public class calclate_3 {
+    public static void main(String[] args){
+        String name = "モリタ";
+        String message;
+        message = name + "です。よろっ";
+        System.out.println(message);
+    }
+}
