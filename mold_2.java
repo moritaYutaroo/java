78771569e4c4943b684b626cd395b354b118f9d2
moritaYutaroo <morita1998@icloud.com>
2024-07-29 11:30:39 +0900

    変数を変えられたくないとき

---
+++ mold_2.java
@@ -0,0 +1,9 @@
+public class mold_2 {
+    public static void main(String[] args){
+        // 動作中に置き換わる必要のないもの
+        final String name = "モリタ";
+        System.out.println(name + "です。");
+        // name = "斎藤"; // エラーが出る
+        System.out.println(name + "です。");
+    }
+}
