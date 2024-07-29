086493e00bc1e9f7053f0ffbc8f7b4582b442d61
moritaYutaroo <morita1998@icloud.com>
2024-07-29 11:31:35 +0900

    様々な型紹介

---
+++ mold.java
@@ -0,0 +1,10 @@
+public class mold {
+    public static void main(String[] args) {
+        int salary = 100000;
+        double height = 180.5;
+        boolean isMarried = true;
+        System.out.println("salary = " + salary);
+        System.out.println("height = " + height);
+        System.out.println("Tom is married: " + isMarried);
+    }
+}
