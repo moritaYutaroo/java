a87bf9870d9f89106ac9c1ac7d38a16356b1085c
moritaYutaroo <morita1998@icloud.com>
2024-07-29 11:31:14 +0900

    練習問題＋printlnで複数の値を入れたい場合

---
+++ mold_practice.java
@@ -0,0 +1,15 @@
+public class mold_practice {
+    public static void main(String[] args) {
+        // 1_2
+        int a = 3;
+        int b = 5;
+        System.out.println("a * b = " + (a * b));
+        // 1_3
+        boolean step1 = true;
+        String step2 = "駆";
+        double step3 = 3.14;
+        long step4 = 314159265853979L;
+        String step5 = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+        System.out.println(step1+","+step2+","+step3+","+step4+","+step5);
+    }
+}
