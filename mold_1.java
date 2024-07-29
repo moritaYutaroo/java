62bdac1d090c895c6550582617282c72c31de8f1
moritaYutaroo <morita1998@icloud.com>
2024-07-29 11:30:24 +0900

    型の上書き

---
+++ mold_1.java
@@ -0,0 +1,9 @@
+public class mold_1 {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("モリタの年齢は " + age);
+        age = 21;
+        System.out.println("モリタの本当の年齢は " + age);
+
+    }
+}
