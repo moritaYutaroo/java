cd9953cba068d821b2d83e5691ee613ff4d93d3d
moritaYutaroo <morita1998@icloud.com>
2024-07-29 11:08:36 +0900

    宣言＋代入＝初期化(値を代入時は型はいらない。)

--- Print_4.java
+++ Print_4.java
@@ -3,6 +3,9 @@ public class Print_4 {
         System.out.println("モリタです。");
         System.out.println("変数を入れて計算してみます。");
         int a = 31;
+        System.out.println("a = " + a);
+        a = 100;
+        System.out.println("a = " + a);
         int b = a+1;
         int c  = a+b;
         System.out.println("a + b = " + a+b );
