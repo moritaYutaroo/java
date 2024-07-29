bacd5a35055eeb0109d6104f94375314fcfc6b75
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:12:04 +0900

    プリント文改行するか否か

---
+++ calclate_4.java
@@ -0,0 +1,12 @@
+public class calclate_4 {
+    public static void main(String[] args){
+        String name = "モリタ";
+        String message;
+        message = name + "です。";
+        System.out.print(message);
+        System.out.print("よろしくお願いします。");
+        // 改行するかしないかの違い
+        System.out.println("よろしくお願いします。");
+
+    }
+}
