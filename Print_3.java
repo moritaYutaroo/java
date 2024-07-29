7f5cc89dfc421a85329fb0221b08055fb1947336
moritaYutaroo <morita1998@icloud.com>
2024-07-29 10:51:44 +0900

    format文追加

---
+++ Print_3.java
@@ -0,0 +1,21 @@
+public class Print_3 {
+    public static void main(String[] args){
+        System.out.println("31+31=");
+        System.out.println(31+31);
+    }
+    public static void sub(String[] args){
+        int a = 31;
+        int b = 31;
+        System.out.printf("%d+%d=%d\n", a, b, a + b);
+    }
+
+}
+
+// // 横道_pythonのf文みたいなことをしたい。(一つのファイルに一つのクラスが原則？)
+// public class Print_4 {
+//     public static void main(String[] args){
+//         int a = 31;
+//         int b = 31;
+//         System.out.printf("%d+%d=%d\n", a, b, a + b);
+//     }
+// }
