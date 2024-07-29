3df36c6d33d391bf28460700e29efb49fcc4f9c0
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:13:24 +0900

    特殊文字入力

---
+++ calculate.java
@@ -0,0 +1,12 @@
+public class calculate {
+    public static void main(String[] args){
+        int a; int b; int c;
+        char d = 'd';String e = "eeee";String f = "\\";
+        a = 31;
+        b = a+1;
+        c = a+b;
+        System.out.println("a="+a+","+"b="+b+","+"c="+c);
+        System.out.println(d+e+f);
+        System.out.println("特殊文字を入力する方法は\"です");
+    }
+}
