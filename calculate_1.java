cef92771b4f555d5e24ea56e8a956ff1b305171c
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:11:02 +0900

    四則演算

---
+++ calculate_1.java
@@ -0,0 +1,13 @@
+public class calculate_1 {
+    public static void main(String[] args){
+        int a; 
+        a = 31;
+        System.out.println("a="+a);
+        a %= 2;
+        System.out.println("a="+a);
+        float g = 3;
+        System.out.println("f="+g);
+        double h = g;
+        System.out.println("h="+h);
+    }
+}
