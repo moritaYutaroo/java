1754fe218379a86274bc73b07026328df9b5a19c
moritaYutaroo <morita1998@icloud.com>
2024-07-29 16:12:34 +0900

    max関数、Stinrg→intに変換

---
+++ calculate_5.java
@@ -0,0 +1,11 @@
+public class calculate_5 {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        int result = Math.max(a, b);
+        System.out.println("aとbのうち、大きいほうは："+result);
+        String age = "20";
+        int modify_age = Integer.parseInt(age);
+        System.out.println("モリタの年齢は " + (6+modify_age));
+    }
+}
