128b3e6aae3899ab068b0d4dbc504672975623dd
moritaYutaroo <morita1998@icloud.com>
2024-07-30 16:26:19 +0900

    listを使ってみた。

---
+++ sequences_2.java
@@ -0,0 +1,14 @@
+public class sequences_2 {
+    // public static void main(String[] args) {
+    //     int [] numbers = {1, 2, 3, 4, 5};
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         System.out.println("numbers[" + i + "] = " + numbers[i]);
+    //     }
+    // }
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.println("number = " + number);
+        }
+    }
+}
