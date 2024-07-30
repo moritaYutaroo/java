a21573dc9adf36dc778bc2dda21640fda16d7372
moritaYutaroo <morita1998@icloud.com>
2024-07-30 13:55:15 +0900

    if文　複数条件

---
+++ branch_4.java
@@ -0,0 +1,11 @@
+public class branch_4 {
+    public static void main(String[] args){
+        String name = "モリタ";
+        String judge_name = "斎藤";
+        if (name.equals(judge_name) || name.equals("モリタ")) {
+            System.out.println("名前が一致しました。");
+        } else {
+            System.out.println("名前が一致しませんでした。");
+        }
+    }
+}
