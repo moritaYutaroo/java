833038048147d5d0b3ba54e03b76c06eb30c2cb2
moritaYutaroo <morita1998@icloud.com>
2024-07-30 16:27:23 +0900

    listの長さ

---
+++ sequences_5.java
@@ -0,0 +1,10 @@
+public class sequences_5 {
+    public static void main(String[] args) {
+        int [] scores = {20, 30, 40, 50, 80};
+        int length = scores.length;
+        // int [] scores;
+        // scores = new int [60];
+        // int length = scores.length;
+        System.out.println(length);
+    }
+}
