3c1eb4e97a8243dbc22fcc1a46209919f00652a2
moritaYutaroo <morita1998@icloud.com>
2024-07-30 13:57:23 +0900

    if文

---
+++ branch.java
@@ -0,0 +1,11 @@
+public class branch {
+    public static void main(String[] args){
+        boolean tenki = true;
+        if (tenki) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("DVDを見ます");
+        }
+    }
+}
