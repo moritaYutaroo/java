fd0261832287a57092760e196e34329971e5d01b
moritaYutaroo <morita1998@icloud.com>
2024-07-30 13:54:50 +0900

    for文_チュートリアル

---
+++ branch_2.java
@@ -0,0 +1,9 @@
+public class branch_2 {
+    public static void main(String[] args){
+        int finish = 0;
+        while (finish < 1000) {
+            System.out.println(finish+"回目の処理です。");
+            finish++;
+        }
+    }
+}
